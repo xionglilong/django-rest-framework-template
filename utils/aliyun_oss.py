9c9da65fae43006f93bd2e6f8fda81c9ea8dce86
熊黎龙 <xionglilong@vip.qq.com>
2022-07-16 12:27:06 +0800

    feat(all): 更新了功能

--- utils/aliyun_oss.py
+++ utils/aliyun_oss.py
@@ -20,7 +20,7 @@ class AliyunOSS:
             AliyunOSS.__is_init = True
 
             # 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-            self.auth = oss2.Auth('LTAI5tNPB5jVFqe3gwGcMwQs', 'O6Kt5ACMT31BJ4oBBRSQVrVlcAs9SP')
+            self.auth = oss2.Auth('XXXX', 'XXX')
             # yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
             self.endpoint = 'https://oss-cn-hangzhou.aliyuncs.com'
             # 填写Bucket名称。
